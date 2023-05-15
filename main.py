26909fcbda35658437e71ba60d3409f34fc43390
Ahmed Ashraf <118582010+14197@users.noreply.github.com>
2023-05-15 01:12:23 +0000

    cleaning the project

---
+++ main.py
@@ -0,0 +1,29 @@
+# Starting with import
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# Let get the data ready: df -> dataframe
+df_1 = pd.read_csv('data1.csv', index_col=0)
+df_2 = pd.read_csv('data2.csv', index_col=0)
+df_3 = pd.read_csv('data3.csv', index_col=0)
+df_4 = pd.read_csv('data4.csv', index_col=0)
+df_5 = pd.read_csv('data5.csv', index_col=0)
+df_6 = pd.read_csv('data6.csv', index_col=0)
+df_7 = pd.read_csv('data7.csv', index_col=0)
+df_8 = pd.read_csv('data8.csv', index_col=0)
+df_9 = pd.read_csv('data9.csv', index_col=0)
+df_10 = pd.read_csv('data10.csv', index_col=0)
+df_11= pd.read_csv('data11.csv', index_col=0)
+df_12 = pd.read_csv('data12.csv', index_col=0)
+df_13 = pd.read_csv('data13.csv', index_col=0)
+df_14 = pd.read_csv('data14.csv', index_col=0)
+df_15 = pd.read_csv('data15.csv', index_col=0)
+df_16 = pd.read_csv('data16.csv', index_col=0)
+df_17 = pd.read_csv('data17.csv', index_col=0)
+df_18 = pd.read_csv('data18.csv', index_col=0)
+df_19 = pd.read_csv('data19.csv', index_col=0)
+df_20 = pd.read_csv('data20.csv', index_col=0)
+df_21 = pd.read_csv('data21.csv', index_col=0)
+
+# Data code: continuing soon
